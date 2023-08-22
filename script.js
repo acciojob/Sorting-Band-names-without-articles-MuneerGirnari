484193ae13d70f7b1fa617c653d28b8691f01197
Muneer Girnari <muneeragirnari@gmail.com>
2023-08-23 00:25:22 +0530

    modified script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let map = {};
+let arr2 = [];
+for(let i in arr){
+    let result = arr[i].replace(/\ba *\b|\ban *\b|\bthe *\b/gi,"");
+    map[result] = arr[i];
+    arr2.push(result);
+}
+
+arr2.sort();
+
+for(let i=0;i<arr2.length;i++){
+    arr2[i] = map[arr2[i]];
+}
+
+
 
